144fdf1b7db8f188ac37ff1beaea7751e31de0a1
Camila Onofri <onofricamila2@gmail.com>
2018-12-07 13:03:51 -0300

    configuration screen added

--- app/src/main/java/labo2018/razasypelajesonofri/MainActivity.java
+++ app/src/main/java/labo2018/razasypelajesonofri/MainActivity.java
@@ -2,6 +2,13 @@ package labo2018.razasypelajesonofri;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set defaults values
+        PreferenceManager.setDefaultValues(this, R.xml.pref_ajustes, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_reconocimiento, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_interaccion, false);
+
+        // get the setting as a SharedPreferences object
+        SharedPreferences sharedPref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String marketPref = sharedPref.getString("market", "-1");
+        Log.d("!!!!MARKET-PREF : ", marketPref);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
